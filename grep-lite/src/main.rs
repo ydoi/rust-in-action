110d1d3a3976e380e333fb1f6dbd9f6fc9c7e206
doi <doi@mmmcorp.co.jp>
2021-12-16 23:54:06 +0900

    [update]grep-lite

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,7 +1,17 @@
 use regex::Regex;
+use clap::{App,Arg};
 
 fn main() {
-  let re = Regex::new("picture").unwrap();
+  let args = App::new("grep-lite")
+    .version("0.1")
+    .about("searches for patterns")
+    .arg(Arg::with_name("pattern")
+      .help("The pattern to search for")
+      .takes_value(true)
+      .required(true))
+    .get_matches();
+  let pattern = args.value_of("pattern").unwrap();
+  let re = Regex::new(pattern).unwrap();
 
   let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
